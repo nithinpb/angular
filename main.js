bbb4c714f749f9b983d103c6f013a404ff5ef297
Nithin Betegeri <nithinpb@gmail.com>
2013-08-09 21:16:39 -0400

    angular first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+function FirstCtrl ($scope) {
+	$scope.items = [
+	{title: 'Paint', quantity: 8, price: 3.85},
+	{title: 'Poll', quantity: 3, price: 4.5},
+	];
+
+	$scope.remove = function(index){
+		$scope.items.splice(index, 1);
+	}
+}
+
+alert("Hi");
